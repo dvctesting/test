914e882fc36930103e778ff8c67c5ed43084764e
DVC Tensorflow <91116182+dvctesting@users.noreply.github.com>
2021-09-27 12:08:11 +0530

    Add files via upload

---
+++ tf_keras.py
@@ -0,0 +1,45 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import tensorflow_cloud as tfc
+
+tfc.run(
+    entry_point=None,
+    distribution_strategy='auto',
+    requirements_txt='requirements.txt',
+    chief_config=tfc.MachineConfig(
+            cpu_cores=8,
+            memory=30,
+            accelerator_type=tfc.AcceleratorType.NVIDIA_TESLA_T4,
+            accelerator_count=2),
+    worker_count=0)
+
+datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+mnist_train, mnist_test = datasets['train'], datasets['test']
+
+num_train_examples = info.splits['train'].num_examples
+num_test_examples = info.splits['test'].num_examples
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+train_dataset = mnist_train.map(scale).cache()
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(
+        28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['accuracy'])
+model.fit(train_dataset, epochs=12)
